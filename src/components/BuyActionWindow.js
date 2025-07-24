e0aa58ca27df3096c54f6e5c09e201d8cfe5a463
Saswata Kumar Nayak <saswatnyk123@gmail.com>
2025-07-24 21:38:38 +0530

    user dashboard of quantnest

---
+++ src/components/BuyActionWindow.js
@@ -0,0 +1,8 @@
+// src/components/BuyActionWindow.js
+import React from 'react';
+
+const BuyActionWindow = ({ uid }) => {
+  return <div>Buy action for {uid}</div>;
+};
+
+export default BuyActionWindow;
